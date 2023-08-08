7deb9d57a7dd77a396cab2856a7e3a8541e9cb3a
Aaban Nayeem <aabannayeem4358@gmail.com>
2023-08-08 11:24:53 +0600

    first commit

---
+++ problem-01.js
@@ -0,0 +1,23 @@
+//^ Sample input - 01
+// The makes Jim has got
+let Jim1 = 84;
+// The marks Dela has got
+let Dela1 = 75;
+
+if (Jim1 > Dela1) {
+  console.log("Jim will get this cake.");
+} else {
+  console.log("Dela will get the cake.");
+}
+
+//^ Sample Input 02
+// The makes Jim has got
+let Jim2 = 69;
+// The markes Dela has got
+let Dela2 = 97;
+
+if (Jim2 > Dela2) {
+  console.log("Jim will get this cake.");
+} else {
+  console.log("Dela will get this cake.");
+}
---
+++ problem-02.js
@@ -0,0 +1,13 @@
+//^ Sample Inpute - 01
+let Jim1 = 84;
+let Dela1 = 99;
+let Chinku1 = 77;
+
+if (Jim1 > Dela1 && Jim1 > Chinku1) {
+  console.log("Jim will get the delicious cake");
+} else if (Dela1 > Jim1 && Dela1 > Chinku1) {
+  console.log("Dela will get the delicious cake");
+} else {
+  console.log("Chinku will get the delicious cake");
+}
+//^ Sample Inpute - 02
